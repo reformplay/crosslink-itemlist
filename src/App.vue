c617005b1cdfa32d324b415071ab252b982a6643
reformplay <joker.kwbt@gmail.com>
2021-02-14 17:51:26 +0900

    データ更新

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,19 @@
     <v-main>
       <Table/>
     </v-main>
+    <Footer/>
   </v-app>
 </template>
 
 <script>
 import Table from './components/table';
-
+import Footer from './components/footer';
 export default {
   name: 'App',
 
   components: {
     Table,
+    Footer
   },
 
   data: () => ({
---
+++ src/components/footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-card height="150px">
+    <v-footer
+      fixed
+    >
+      <v-card
+        flat
+        tile
+        width="100%"
+        class="indigo lighten-1 text-center"
+      >
+        <v-card-text class="white--text">
+          マーケット活性化を願って作成しました
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-text class="white--text">
+          投げ銭：IOST ( <strong>reformplay</strong> ) or Linkから購入（紹介料いただけます！）
+        </v-card-text>
+      </v-card>
+    </v-footer>
+
+    
+  </v-card>
+</template>
