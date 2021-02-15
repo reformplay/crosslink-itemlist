073fe77be4a2b16401aef92f75a9001a34e4faa8
川畑 <yuta-kawabata@bbss.co.jp>
2021-02-15 17:50:30 +0900

    ga pluginの変更

--- src/main.js
+++ src/main.js
@@ -3,12 +3,12 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
-import VueAnalytics from 'vue-analytics';
+import VueGtag from 'vue-gtag';
 
 Vue.config.productionTip = false
 Vue.use(VueAxios, axios);
-Vue.use(VueAnalytics,{
-  id: 'G-7HJRY0FFFS'
+Vue.use(VueGtag,{
+  config:{id: 'G-7HJRY0FFFS'}
 });
 
 new Vue({
