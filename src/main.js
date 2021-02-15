effddb72ef05a96d65561279043b4e3efe1777a9
川畑 <yuta-kawabata@bbss.co.jp>
2021-02-15 17:44:50 +0900

    google analytics追加

--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
+import VueAnalytics from 'vue-analytics';
 
 Vue.config.productionTip = false
 Vue.use(VueAxios, axios);
+Vue.use(VueAnalytics,{
+  id: 'G-7HJRY0FFFS'
+});
 
 new Vue({
   vuetify,
